839e3808191a79ec6489b892973125665fda6965
bb-centrum <benbridges@centrumpile.co.uk>
2022-09-28 17:54:47 +0100

    Add solutions

--- react-fundamentals/scripts/setup.js
+++ react-fundamentals/scripts/setup.js
@@ -16,16 +16,16 @@ function color(modifier, string) {
 
 console.log(color('info', '▶️  Starting workshop setup...'))
 
-var error = spawnSync('npx --version', {shell: true}).stderr.toString().trim()
-if (error) {
-  console.error(
-    color(
-      'danger',
-      '🚨  npx is not available on this computer. Please install npm@5.2.0 or greater',
-    ),
-  )
-  throw error
-}
+// var error = spawnSync('npx --version', {shell: true}).stderr.toString().trim()
+// if (error) {
+//   console.error(
+//     color(
+//       'danger',
+//       '🚨  npx is not available on this computer. Please install npm@5.2.0 or greater',
+//     ),
+//   )
+//   throw error
+// }
 
 var command =
   'npx "https://gist.github.com/kentcdodds/bb452ffe53a5caa3600197e1d8005733" -q'
--- react-fundamentals/src/exercise/06.js
+++ react-fundamentals/src/exercise/06.js
@@ -4,6 +4,10 @@
 import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
+  function handleSubmit(event){
+    event.preventDefault()
+    onSubmitUsername(event.target.elements[0].value)
+  }
   // 🐨 add a submit event handler here (`handleSubmit`).
   // 💰 Make sure to accept the `event` as an argument and call
   // `event.preventDefault()` to prevent the default behavior of form submit
@@ -20,9 +24,9 @@ function UsernameForm({onSubmitUsername}) {
   // 🐨 make sure to associate the label to the input.
   // to do so, set the value of 'htmlFor' prop of the label to the id of input
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
-        <label>Username:</label>
+        <label htmlFor="usernameInput">Username:</label>
         <input type="text" />
       </div>
       <button type="submit">Submit</button>
