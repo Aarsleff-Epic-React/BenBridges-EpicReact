5a7024fdc1192e5e5c5b1eb5f886d18d63df99f4
bb-centrum <benbridges@centrumpile.co.uk>
2022-10-18 12:08:52 +0100

    Complete exercise 1 ex 4

--- advanced-react-hooks/src/exercise/01.js
+++ advanced-react-hooks/src/exercise/01.js
@@ -3,23 +3,33 @@
 
 import * as React from 'react'
 
+// function countReducer(state, action) {
+//   return {
+//   ...state,
+//   ...(typeof action === 'function' ? action(state) : action)
+//   }
+// }
+
 function countReducer(state, action) {
-  return {
-  ...state,
-  ...(typeof action === 'function' ? action(state) : action)
+  switch (action.type){
+    case 'INCREMENT': {
+      return {count: state.count + action.step}
+    }
+    default : {
+      break
+    }
   }
 }
 
-function Counter({initialCount = 0, step = 1}) {
+function Counter({initialCount = 0, step = 3}) {
   // 🐨 replace React.useState with React.useReducer.
   // 💰 React.useReducer(countReducer, initialCount)
   // const [count, setCount] = React.useState(initialCount)
-  const [state, setState] = React.useReducer(countReducer, {
+  const [state, dispatch] = React.useReducer(countReducer, {
     count: initialCount,
   })
   const {count} = state
-  const increment = () =>
-    setState(currentState => ({count: currentState.count + step}))
+  const increment = () => dispatch({type: 'INCREMENT', step})
   return <button onClick={increment}>{count}</button>
 }
 
